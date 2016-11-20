c2bb80f3fa6d6924754e5d58c76d044cfbccbb96
griffiw4 <griffiw4@my.erau.edu>
2016-11-20 14:59:07 -0500

    update recevier code

--- PI_Balloon_Code_Slave_Reader.c
+++ PI_Balloon_Code_Slave_Reader.c
@@ -8,6 +8,42 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int main(int argc, char* argv[]) { 
-	printf("Hello HELL");
+// The PiWeather board i2c address
+#define ADDRESS 0x08
+
+// The I2C bus: This is for V2 pi's. For V1 Model B you need i2c-0
+static const char *devName = "/dev/i2c-1";
+
+
+char buf[10];
+
+//argc: the number of argments sent to main()
+//argv: the actual arguments
+int main(int argc, char* argv[]) {
+
+	printf("I2C: Connecting\n");
+	int file;
+
+	if ((file = open(devName, O_RDWR)) < 0) {
+		fprintf(stderr, "I2C: Failed to access %d\n", devName);
+		exit(1);
+	}
+
+	printf("I2C: acquiring buss to 0x%x\n", ADDRESS);
+
+	if (ioctl(file, I2C_SLAVE, ADDRESS) < 0) {
+		fprintf(stderr, "I2C: Failed to acquire bus access/talk to slave 0x%x\n", ADDRESS);
+		exit(1);
+	}
+	
+	req.tv_nsec = 500000000; //500ms
+	
+	while (1){
+		read(file, buf, 36);
+		printf("%s\n", buf);
+		nanosleep(req,rem);
+	}
+
+	close(file);
+	return (EXIT_SUCCESS);
 }
