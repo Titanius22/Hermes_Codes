d00fbe0c6617c09905f1c570d7080ce7245cd05e
griffiw4 <griffiw4@my.erau.edu>
2016-11-20 14:06:34 -0500

    Testing I2C reading code

---
+++ PI_Balloon_Code_Slave_Reader.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) { 
+	printf("Hello HELL");
+}
