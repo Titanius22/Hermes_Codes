fae4c1dbb6f9f39d0d87436f82402cc6970c69c6
Titanius22 <erikparker2013@gmail.com>
2016-11-30 21:29:00 -0500

    Gearing up for Dr. B test.

--- PI_Balloon_Code_Slave_Reader.c
+++ PI_Balloon_Code_Slave_Reader.c
@@ -9,7 +9,13 @@
 #include <unistd.h>
 
 // The PiWeather board i2c address
-#define ADDRESS 0x08
+#define ADDRESS 0x04
+
+
+//Prototyping
+int getIntFromByte(unsigned char** ,short );
+
+
 
 // The I2C bus: This is for V2 pi's. For V1 Model B you need i2c-0
 static const char *devName = "/dev/i2c-1";
@@ -37,14 +43,59 @@ int main(int argc, char* argv[]) {
 		exit(1);
 	}
 	
-	req.tv_nsec = 500000000; //500ms
+	req.tv_nsec = 50000000; //50ms
 	
 	while (1){
-		read(file, buf, 36);
-		printf("%s\n", buf);
+		read(file, buf, 22);
+		
+		char* writeArray=buf;
+		char** wrPtr=&writeArray;
+		printf((unsigned int)getIntFromByte(wrPtr,3));
+		  
+		printf((unsigned int)getIntFromByte(wrPtr,4));
+		  
+		printf((unsigned int)getIntFromByte(wrPtr,4));
+
+		printf((unsigned int)getIntFromByte(wrPtr,3));
+
+		printf((unsigned int)getIntFromByte(wrPtr,2));
+		  
+		printf((unsigned int)getIntFromByte(wrPtr,3));
+
+		printf((char)getIntFromByte(wrPtr,1));
+
+		printf((char)getIntFromByte(wrPtr,1));
+
+		printf((char)getIntFromByte(wrPtr,1));
+		
+		
+		//printf("%s\n", buf);
 		nanosleep(&req,&rem);
 	}
 
 	close(file);
 	return (EXIT_SUCCESS);
+}
+
+long getIntFromByte(unsigned char** arrayStart, short bytes){
+
+  //Allocating array to read into
+  char* intPtr = malloc (sizeof(unsigned long));
+  unsigned long temp;
+  //Void pointer to same location to return
+
+   //Loop Counter
+  short loopCount;
+  for(loopCount=0;loopCount<bytes;loopCount++){
+
+    //Copying bytes from one array to the other
+    if(loopCount<bytes){
+      intPtr[loopCount]=(*arrayStart)[loopCount];
+    }
+  }
+  *arrayStart+=(short)bytes;
+  temp=*((unsigned long*)intPtr);
+  free(intPtr);
+  //Returning void pointer (Pointer to an integer with the designated of the number of bytes)
+  return temp;
 }
