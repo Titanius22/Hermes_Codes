b331dd2651e5a042d84563d2dd1f8175c7d733ef
Titanius22 <Titanius22@users.noreply.github.com>
2017-02-07 10:16:36 -0500

    Use sprintf for I2C comms to ground station Arduino
    This will very mildly slow down comms but will allow us to progress on other things

--- Client.c
+++ Client.c
@@ -16,11 +16,13 @@
 //prototyping
 unsigned long getIntFromByte(unsigned char** , short);
 void insertBytesFromInt(void* ,unsigned char** , short);
-int tryNewSocketConnection(int);
+int tryNewSocketConnection();
 
 //Globals
 int ServerFileNum;
-struct sockaddr_in serv_addr; 
+struct sockaddr_in serv_addr;
+int startingSocketNum = 5000;
+bool madeConnection = false; //becomes true when connection is made. If connection is lost afterwards (meaning when madeConnection is true), the port number is incremented and madeConnection is set to false till another connection is found.
 
 // The slave Arduino address
 #define ADDRESS 0x04
@@ -32,7 +34,6 @@ int main(int argc, char *argv[])
 {
     char recvBuff[200];
 	int n = 0;
-	int startingSocketNum = 5000;
 	struct timespec req={0},rem={0};
 	req.tv_nsec = 500000000; //500ms
 
@@ -68,17 +69,23 @@ int main(int argc, char *argv[])
 	char fileName[] = "DistanceTest";
 	//char mostFilePath[] = "/home/alarm/randomJunk/cCodeTests/";
 	char fileExt[] = ".txt";
-	char fullFilePath[60];
+	char fullFilePath[80];
 	FILE *filePointer;
 	char leftOvers[25];
 	char* writeArray;
 	char** wrPtr;
-	char command = 1;
+	char command[25];
+	int madeConnection = 0; //becomes 1 when connection is made. If connection is lost afterwards (meaning when madeConnection is 1), the port number is incremented and madeConnection is set to 0 till another connection is found.
+	
+	unsigned int DataLineCounter;
+	unsigned long DataGPS[3]; // Longitude, Latitude, Altitude
+	unsigned int DataSensors[8]; // External Thermistor, Battery Voltage, Magnotometer X, Y, Z, Humidity, Pressure, Internal Temperature.
+	char DataEndLine[3];
 	
 	while(1){ 
 		
 		// If connection was made properly
-		if(ServerFileNum = tryNewSocketConnection(startingSocketNum) >= 0){
+		if(tryNewSocketConnection() == 0){
 		
 		
 			//while ( (n = recv(ServerFileNum, recvBuff, 32 , 0)) > 0) same as read if last argument is 0
@@ -114,39 +121,55 @@ int main(int argc, char *argv[])
 					char* writeArray=recvBuff;
 					char** wrPtr=&writeArray;
 					
-					printf("%d ", (unsigned int)getIntFromByte(wrPtr,3)); // Line Counter
+					DataLineCounter = (unsigned int)getIntFromByte(wrPtr,3);
+					printf("%d ", DataLineCounter); // Line Counter
 			  
-					printf("%lu ", (unsigned long)getIntFromByte(wrPtr,5)); // Longitude
+					DataGPS[0] = (unsigned long)getIntFromByte(wrPtr,5);
+					printf("%lu ", DataGPS[0]); // Longitude
 			  
-					printf("%lu ", (unsigned long)getIntFromByte(wrPtr,5)); // Latitude
+					DataGPS[1] = (unsigned long)getIntFromByte(wrPtr,5);
+					printf("%lu ", DataGPS[1]); // Latitude
 
-					printf("%d ", (unsigned int)getIntFromByte(wrPtr,3)); // Altitude
+					DataGPS[2] = (unsigned int)getIntFromByte(wrPtr,3);
+					printf("%d ", DataGPS[2]); // Altitude
 
-					printf("%d ", (unsigned int)getIntFromByte(wrPtr,2)); // External Thermistor
+					DataSensors[0] = (unsigned int)getIntFromByte(wrPtr,2);
+					printf("%d ", DataSensors[0]); // External Thermistor
 			  
-					printf("%d ", (unsigned int)getIntFromByte(wrPtr,1)); // Battery Voltage
+					DataSensors[1] = (unsigned int)getIntFromByte(wrPtr,1);
+					printf("%d ", DataSensors[1]); // Battery Voltage
 
-					printf("%d ", (unsigned int)getIntFromByte(wrPtr,1)); // Magnotometer X
+					DataSensors[2] = (unsigned int)getIntFromByte(wrPtr,1);
+					printf("%d ", DataSensors[2]); // Magnotometer X
 
-					printf("%d ", (unsigned int)getIntFromByte(wrPtr,1)); // Magnotometer Y
+					DataSensors[3] = (unsigned int)getIntFromByte(wrPtr,1);
+					printf("%d ", DataSensors[3]); // Magnotometer Y
 
-					printf("%d ", (unsigned int)getIntFromByte(wrPtr,1)); // Magnotometer Z
+					DataSensors[4] = (unsigned int)getIntFromByte(wrPtr,1);
+					printf("%d ", DataSensors[4]); // Magnotometer Z
 
-					printf("%d ", (unsigned int)getIntFromByte(wrPtr,1)); // Humidity
+					DataSensors[5] = (unsigned int)getIntFromByte(wrPtr,1);
+					printf("%d ", DataSensors[5]); // Humidity
 
-					printf("%d ", (unsigned int)getIntFromByte(wrPtr,4)); // Pressure
+					DataSensors[6] = (unsigned int)getIntFromByte(wrPtr,4);
+					printf("%d ", DataSensors[6]); // Pressure
 
-					printf("%d ", (unsigned int)getIntFromByte(wrPtr,2)); // Internal Temperature
+					DataSensors[7] = (unsigned int)getIntFromByte(wrPtr,2);
+					printf("%d ", DataSensors[7]); // Internal Temperature
 					
-					printf("%c", (char)getIntFromByte(wrPtr,1)); // 'E'
+					DataEndLine[0] = (char)getIntFromByte(wrPtr,1);
+					printf("%c", DataEndLine[0]); // 'E'
 
-					printf("%c", (char)getIntFromByte(wrPtr,1)); // 'N'
+					DataEndLine[1] = (char)getIntFromByte(wrPtr,1);
+					printf("%c", DataEndLine[1]); // 'N'
 
-					printf("%c\n", (char)getIntFromByte(wrPtr,1)); // 'D'
+					DataEndLine[2] = (char)getIntFromByte(wrPtr,1);
+					printf("%c\n", DataEndLine[2]); // 'D'
 					
 					// Send data over I2C
-					write(i2cFile, command, 1);
-					write(i2cFile, recvBuff+3, 13);
+					
+					sprintf(command, "1%lu%lu%d", DataGPS[0], DataGPS[1], DataGPS[2]);
+					write(i2cFile, command, strlen(command));
 					
 					// File tracking and counting
 					fileLineCount = 0;
@@ -172,18 +195,26 @@ int main(int argc, char *argv[])
 }
 
 //SERVER STUFF. setting up socket
-void tryNewSocketConnection(int socketNum){
-		
-    if((ServerFileNum = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+int tryNewSocketConnection(){
+	
+	if (madeConnection == 1){
+		startingSocketNum++;
+		madeConnection = 0;
+	}
+	
+	// Makes varible free
+	serv_addr = {0};
+	
+	if((ServerFileNum = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Error : Could not create socket \n");
         return -1;
-    } 
+    }
 
     memset(&serv_addr, '0', sizeof(serv_addr)); 
 
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(socketNum); 
+    serv_addr.sin_port = htons(startingSocketNum); 
 
     // The arv[1] was originally the first trminal argument which was the ip address 
 	//if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
@@ -198,6 +229,10 @@ void tryNewSocketConnection(int socketNum){
 		printf("\n Error : Connect Failed \n");
 		return -1;
 	}
+	
+	madeConnection = 1;
+	
+	return 0;
 }
 		
 unsigned long getIntFromByte(unsigned char** arrayStart, short bytes){
