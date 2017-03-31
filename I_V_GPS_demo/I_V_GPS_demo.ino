a0ef477ebe3c6a2a02d80045ba6f103f07bc6fa6
Titanius22 <erikparker2013@gmail.com>
2017-03-31 11:01:25 -0400

    Moved updateCharsToSend() within the main loop

--- I_V_GPS_demo/I_V_GPS_demo.ino
+++ I_V_GPS_demo/I_V_GPS_demo.ino
@@ -51,8 +51,8 @@ unsigned long long loloballoonLon             = 20;//91.6458;//[degrees]
 unsigned long longballoonAlt             = 30;//10064.0;///5280.0;//[miles]
 
 // for Housekeeping------------------------------------------------------------------------------------------
-int Vcount;
-int Icount;
+unsigned char Vcount;
+unsigned char Icount;
 
 //const byte rxPin = 0;
 //const byte txPin = 1;
@@ -82,50 +82,48 @@ bool newdata = false;
 unsigned short LineLength = 29; //excludes checksum byte
 
 void setup() {
-  // for communication with Pi
-  Wire.begin(4);
-  
-  
-  //for GPS------------------------------------------------------------------------------------------
-  Serial.begin(74880);
-  Serial1.begin(9600);                     // Communicate at 9600 baud (default for PAM-7Q module)
-  
-  delay(200);
-  
-  // for Sensor------------------------------------------------------------------------------------------
-  // Disable internal pullups, 10Kohms are on the breakout
-  PORTC |= (1 << 4);
-  PORTC |= (1 << 5);
-  delay(100);
-  
-  //initial(ADDRESS);
-  //GPSstuff(); 
-  //SENSORstuff();
-  //updateCharsToSend();
-  
-  Wire.onRequest(requestEvent); // register event
-  
-  updateCharsToSend();
+	// for communication with Pi
+	Wire.begin(4);
+
+
+	//for GPS------------------------------------------------------------------------------------------
+	Serial.begin(74880);
+	Serial1.begin(9600);                     // Communicate at 9600 baud (default for PAM-7Q module)
+
+	delay(200);
+
+	// for Sensor------------------------------------------------------------------------------------------
+	// Disable internal pullups, 10Kohms are on the breakout
+	PORTC |= (1 << 4);
+	PORTC |= (1 << 5);
+	delay(100);
+
+	//initial(ADDRESS);
+	//GPSstuff(); 
+	//SENSORstuff();
+	//updateCharsToSend();
+
+	Wire.onRequest(requestEvent); // register event
+
+	updateCharsToSend();
   
 }
 
 void loop() {
-  //Serial.print((char) CharsToSend[0]);
-  //Serial.print((char) CharsToSend[1]);
-  //Serial.print((char) CharsToSend[2]);
-  //Serial.print((char) CharsToSend[3]);
-  //Serial.print((char) CharsToSend[4]);
-  //Serial.print((char) CharsToSend[5]);
-  
-  //delay(1000);
-  GPSstuff();
-  houseKeeping();
+	//Serial.print((char) CharsToSend[0]);
+	//Serial.print((char) CharsToSend[1]);
+	//Serial.print((char) CharsToSend[2]);
+	//Serial.print((char) CharsToSend[3]);
+	//Serial.print((char) CharsToSend[4]);
+	//Serial.print((char) CharsToSend[5]);
 
-  //updateCharsToSend();
+	//delay(1000);
+	GPSstuff();
+	houseKeeping();
+
+	//updateCharsToSend();
   
   if(newdata){
-    
-    updateCharsToSend();
     
 	  loloballoonLat = (unsigned long) (balloonLat*100000);
 	  loloballoonLon = (unsigned long) (balloonLon*100000);
@@ -157,38 +155,40 @@ void loop() {
 	  //lineCount; //Wierd. This must be here for linecount to increment in the requestEvent()
 	  //lineCount++; // increments in updateCharsToSend
   }
-
-  Serial.print("LAT: ");
-  Serial.println(balloonLat);
-  Serial.print("LON: ");
-  Serial.println(balloonLon);
-  Serial.print("ALT: ");
-  Serial.println(balloonAlt);
-  Serial.println("");
   
-  delay(500);
+	updateCharsToSend();
+
+	Serial.print("LAT: ");
+	Serial.println(balloonLat);
+	Serial.print("LON: ");
+	Serial.println(balloonLon);
+	Serial.print("ALT: ");
+	Serial.println(balloonAlt);
+	Serial.println("");
+  
+	delay(500);
 }
 
 // function that executes whenever data is requested by master
 // this function is registered as an event, see setup()
 void requestEvent() {
-  Serial.println("requestEvent");
-  //GPSstuff();
-  //SENSORstuff();
-  //char* CharsToSend = updateCharsToSend();
-  //updateCharsToSend();
-  //lineCount++;
-  //updateCharsToSend();
+	Serial.println("requestEvent");
+	//GPSstuff();
+	//SENSORstuff();
+	//char* CharsToSend = updateCharsToSend();
+	//updateCharsToSend();
+	//lineCount++;
+	//updateCharsToSend();
 
-  Wire.write(CharsToSend, 30); // respond with message of 32 byte
-  //Wire.write("W", 1); // respond with message of 32 byte
-  
-  //updateCharsToSend();
-  //free(CharsToSend);
-  //Wire.write("ftgyho04856000r57j0k?0");
-  
-  //GPSstuff();
-  //SENSORstuff();
+	Wire.write(CharsToSend, 30); // respond with message of 32 byte
+	//Wire.write("W", 1); // respond with message of 32 byte
+
+	//updateCharsToSend();
+	//free(CharsToSend);
+	//Wire.write("ftgyho04856000r57j0k?0");
+
+	//GPSstuff();
+	//SENSORstuff();
   
   
 }
@@ -270,106 +270,107 @@ void updateCharsToSend(){
 
 void insertBytesFromInt(void* value,unsigned char** byteStart, short numberBytesToCopy){
 
-  unsigned char* valueBytes=value;
-  short loopCount=0;
-  for(loopCount=0;loopCount<numberBytesToCopy;loopCount++){
-    (*byteStart)[loopCount]=valueBytes[loopCount];
-  }
-  *byteStart+=(short)numberBytesToCopy;
+	unsigned char* valueBytes=value;
+	short loopCount=0;
+	for(loopCount=0;loopCount<numberBytesToCopy;loopCount++){
+	(*byteStart)[loopCount]=valueBytes[loopCount];
+	}
+	*byteStart+=(short)numberBytesToCopy;
 }
 
 unsigned long getIntFromByte(unsigned char** arrayStart, short bytes){
 //unsigned long long getIntFromByte(unsigned char** arrayStart, short bytes){
 
-  //Allocating array to read into
-  unsigned char* intPtr=malloc (sizeof(unsigned long long));
-  unsigned long long temp;
-  //Void pointer to same location to return
+	//Allocating array to read into
+	unsigned char* intPtr=malloc (sizeof(unsigned long long));
+	unsigned long long temp;
+	//Void pointer to same location to return
 
-   //Loop Counter
-  short loopCount;
-  for(loopCount=0;loopCount<bytes;loopCount++){
+	//Loop Counter
+	short loopCount;
+	for(loopCount=0;loopCount<bytes;loopCount++){
 
-    //Copying bytes from one array to the other
-    if(loopCount<bytes){
-      intPtr[loopCount]=(*arrayStart)[loopCount];
-    }
-  }
-  *arrayStart+=(short)bytes;
-  temp=*((unsigned long long*)intPtr);
-  free(intPtr);
-  //Returning void pointer (Pointer to an integer with the designated of the number of bytes)
-  return temp;
+	//Copying bytes from one array to the other
+	if(loopCount<bytes){
+	  intPtr[loopCount]=(*arrayStart)[loopCount];
+	}
+	}
+	*arrayStart+=(short)bytes;
+	temp=*((unsigned long long*)intPtr);
+	free(intPtr);
+	//Returning void pointer (Pointer to an integer with the designated of the number of bytes)
+	return temp;
 }
 
 void GPSstuff() {
-  int64_t start = millis();       // starts a count of millisec since the code began 
-  newdata = false;
-  while (millis() - start < 250) {     // Update every 1 seconds
-    if (feedgps())                      // if serial1 is available and can read gps.encode
-      newdata = true;
-  }
-  if (newdata) {  // if locked
-    gpsdump(gps);
-     
-    
-    //using GPS ------------------------------------------------------------------------------------------
-    Serial.println("LOCKED ON");
-    //Serial.print("Balloon Altitude: ");
-    balloonAlt = gps.altitude(); 
-  }else{          // if not locked
-    Serial.println("Not Locked");
-  }
+	int64_t start = millis();       // starts a count of millisec since the code began 
+	newdata = false;
+	while (millis() - start < 250) {     // Update every 1 seconds
+		if (feedgps()){                    // if serial1 is available and can read gps.encode
+			newdata = true;
+		}
+	}
+	if (newdata) {  // if locked
+		gpsdump(gps);
+		 
+
+		//using GPS ------------------------------------------------------------------------------------------
+		Serial.println("LOCKED ON");
+		//Serial.print("Balloon Altitude: ");
+		balloonAlt = gps.altitude(); 
+	}else{          // if not locked
+		Serial.println("Not Locked");
+	}
 }
 
 // Get and process GPS data
 void gpsdump(TinyGPS &gps) {
-  //unsigned long age;
-  gps.f_get_position(&balloonLat, &balloonLon, &age);
-  //Serial.print(balloonLat, 4); 
-  //Serial.print(", "); 
-  //Serial.println(balloonLon, 4);
+	//unsigned long age;
+	gps.f_get_position(&balloonLat, &balloonLon, &age);
+	//Serial.print(balloonLat, 4); 
+	//Serial.print(", "); 
+	//Serial.println(balloonLon, 4);
 }
 
 // Feed data as it becomes available 
 bool feedgps() {
-  while (Serial1.available()) {
-    if (gps.encode(Serial1.read()))
-      return true;
-  }
-  return false;
+	while (Serial1.available()) {
+		if (gps.encode(Serial1.read()))
+			return true;
+	}
+	return false;
 }
 
 // Feed data as it becomes available 
 void houseKeeping() {
-  int analogPinV = 0;     // potentiometer wiper (middle terminal) connected to analog pin 3outside leads to ground and +5V
-  int analogPinI = 1; 
-  double Vread = 0;           // variable to store the value read
-  double Iread = 0;           // variable to store the value read
-  
-  Vread = round(analogRead(analogPinV)/4);    // read the input pin
-  Iread = round(analogRead(analogPinI)/4);    // read the input pin
-  double Vvoltread = (5*Vread)/256;    // read the input pin
-  double Ivoltread = (5*Iread)/256;    // read the input pin
-  double Vin = 3.2206*Vvoltread - 0.086;    // read the input pin
-  double Iin = 0.1116*Ivoltread - 0.0009;    // read the input pin
-  
-  Serial.print("\nV Value: ");         
-  Serial.print(Vread);      
-  Serial.print(", ");       
-  Serial.print(Vvoltread);   
-  Serial.print(", ");       
-  Serial.print(Vin);          
-  Serial.print("\nI Value: ");    
-  Serial.print(Iread);
-  Serial.print(", ");
-  Serial.print(Ivoltread);    
-  Serial.print(", ");      
-  Serial.print(Iin);         
-  Serial.print("\n");   
-  
-  Vcount = (int) Vread;
-  Icount = (int) Iread;
+	int analogPinV = 0;     // potentiometer wiper (middle terminal) connected to analog pin 3outside leads to ground and +5V
+	int analogPinI = 1; 
+	double Vread = 0;           // variable to store the value read
+	double Iread = 0;           // variable to store the value read
+
+	Vread = round(analogRead(analogPinV)/4);    // read the input pin
+	Iread = round(analogRead(analogPinI)/4);    // read the input pin
+	double Vvoltread = (5*Vread)/256;    // read the input pin
+	double Ivoltread = (5*Iread)/256;    // read the input pin
+	double Vin = 3.2206*Vvoltread - 0.086;    // read the input pin
+	double Iin = 0.1116*Ivoltread - 0.0009;    // read the input pin
+
+	Serial.print("\nV Value: ");         
+	Serial.print(Vread);      
+	Serial.print(", ");       
+	Serial.print(Vvoltread);   
+	Serial.print(", ");       
+	Serial.print(Vin);          
+	Serial.print("\nI Value: ");    
+	Serial.print(Iread);
+	Serial.print(", ");
+	Serial.print(Ivoltread);    
+	Serial.print(", ");      
+	Serial.print(Iin);         
+	Serial.print("\n");   
+
+	Vcount = (unsigned char) Vread;
+	Icount = (unsigned char) Iread;
 }
 
 // void initial(uint8_t address)
